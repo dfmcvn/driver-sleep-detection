62fea203adb71b56f09550c2204e3635cfcea9dc
Vit Bac Ha <dzith@mintduckA15.local>
2024-09-20 12:17:47 +0700

    add fps indicator

--- driver_sleep_detection/_gui.py
+++ driver_sleep_detection/_gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import cv2
+import time
 
 
 class GUI:
@@ -29,7 +30,30 @@ class GUI:
         self.video_label = ttk.Label(self.video_frame)
         self.video_label.pack(expand=True, fill=tk.BOTH)
 
+        # Add FPS label
+        self.fps_label = ttk.Label(
+            self.video_frame, text="FPS: 0", background="black", foreground="white"
+        )
+        self.fps_label.place(x=10, y=10)
+
+        # Initialize FPS calculation variables
+        self.frame_count = 0
+        self.start_time = time.time()
+
     def update_video(self, frame):
+        # Increment frame count
+        self.frame_count += 1
+
+        # Calculate elapsed time
+        elapsed_time = time.time() - self.start_time
+
+        # Update FPS every 1 second
+        if elapsed_time >= 1:
+            fps = self.frame_count / elapsed_time
+            self.fps_label.config(text=f"FPS: {fps:.2f}", foreground="red")
+            self.frame_count = 0
+            self.start_time = time.time()
+
         # Chuyển đổi khung hình từ không gian màu BGR sang RGB
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # Tạo một đối tượng PIL Image từ mảng numpy
